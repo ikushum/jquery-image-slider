67992c27bc93779570181480b9aa51bfe971e3b7
Ishan <ikushum@gmail.com>
2016-09-27 00:21:26 +0545

    Made it more functional

--- ikushum_image_slider.js
+++ ikushum_image_slider.js
@@ -1,18 +1,49 @@
+	var img_number=1;
+	var new_img_number=2;
+	var move="right";
+
 $(document).ready(function(){
 
-	$("img#ikushum_image_1").delay(5000).show();
-	$("img#ikushum_image_1").hide('slide',{direction:'left'},1000);
+	function slide_forward(img_number,new_img_number){
+		$("img#ikushum_image_"+img_number).hide('slide',{direction:'left'},1000);
+		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'right'},1000);
+	}
 
-	var count=2;
+	function slide_backward(img_number,new_img_number){
+		$("img#ikushum_image_"+img_number).hide('slide',{direction:'right'},1000);
+		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'left'},1000);
+	}
 
-	setInterval(function(){
+	$("img#ikushum_image_1").show();
 
-		$("img#ikushum_image_"+count).show('slide',{direction:'right'},1000);
-		$("img#ikushum_image_"+count).delay(4000).hide('slide',{direction:'left'},1000);
+	var interval = setInterval(function(){
 
-		count=count+1;
-		if(count==4)
-			count=1;
+		$(document).keydown(function (e){
+			if(e.keyCode==37){ //left key
+				move="left";
+			}
+  			else if(e.keyCode==39)
+  				move="right";
+  		});
+
+  		if(move=="right"){
+  			if(img_number==3)
+				new_img_number=1;
+			else
+				new_img_number=img_number+1;
+			slide_forward(img_number,new_img_number);
+		}
+		else if(move="left"){
+			if(img_number==1)
+				new_img_number=3;
+			else
+			new_img_number=img_number-1;	
+			slide_backward(img_number,new_img_number);
+		}
+
+  		img_number=new_img_number;
+
+
 	},5000);
 
 });
