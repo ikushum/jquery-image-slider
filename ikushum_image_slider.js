3d31160091d3ab9ff48d39366a9b4a55237517da
Ishan <ikushum@gmail.com>
2016-09-27 21:41:02 +0545

    Improved User Experience

--- ikushum_image_slider.js
+++ ikushum_image_slider.js
@@ -4,7 +4,20 @@ var loop;
 
 $(document).ready(function(){
 	$("img#ikushum_image_1").show();
-	total_image = $("#ikushum_image_slider img").length;
+
+	total_image = $("#ikushum_image_slider img.ikushum_image").length;
+
+	$(".arrow").hover(function(){
+		$(this).stop(true,true).toggleClass('arrow_hover',250);
+	});
+
+    $(document).keydown(function (e){
+    	if(e.keyCode==39) //right key
+    		next();
+    	if(e.keyCode==37) //left key
+    		prev();
+	});
+
 	startslideshow();
 });
 
@@ -22,19 +35,19 @@ function startslideshow(){
 };
 
 function slide_forward(){
-		$("img#ikushum_image_"+img_number).hide('slide',{direction:'left'},1000);
-		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'right'},1000);
+		$("img#ikushum_image_"+img_number).stop(true,true).fadeOut(1000);
+		$("img#ikushum_image_"+new_img_number).stop(true,true).fadeIn(1000);
 		 img_number=new_img_number;
 };
 
 function slide_backward(){
-		$("img#ikushum_image_"+img_number).hide('slide',{direction:'right'},1000);
-		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'left'},1000);
+		$("img#ikushum_image_"+img_number).stop(true,true).fadeOut(1000);
+		$("img#ikushum_image_"+new_img_number).stop(true,true).fadeIn(1000);
  		img_number=new_img_number;
 };
 
 function next(){
-	stoploop();
+	window.clearInterval(loop);	
 	if(img_number==total_image)
 		new_img_number=1;
 	else
@@ -44,18 +57,13 @@ function next(){
 }
 
 function prev(){
-	stoploop();
+	window.clearInterval(loop);	
 	if(img_number==1)
 		new_img_number=total_image;
 	else
 		new_img_number=img_number-1;
 	slide_backward();
 	startslideshow();
-
-}
-
-function stoploop(){
-	window.clearInterval(loop);	
 }
 
 
