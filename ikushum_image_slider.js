d2ca482ae719327692af3bcc454db63f10898feb
Ishan <ikushum@gmail.com>
2016-09-27 12:28:47 +0545

    Added Next and Previous buttons

--- ikushum_image_slider.js
+++ ikushum_image_slider.js
@@ -1,49 +1,66 @@
-	var img_number=1;
-	var new_img_number=2;
-	var move="right";
+var img_number=1;
+var new_img_number=2;
+var loop;
 
 $(document).ready(function(){
-
-	function slide_forward(img_number,new_img_number){
-		$("img#ikushum_image_"+img_number).hide('slide',{direction:'left'},1000);
-		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'right'},1000);
-	}
-
-	function slide_backward(img_number,new_img_number){
-		$("img#ikushum_image_"+img_number).hide('slide',{direction:'right'},1000);
-		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'left'},1000);
-	}
-
 	$("img#ikushum_image_1").show();
+	total_image = $("#ikushum_image_slider img").length;
+	startslideshow();
+});
 
-	var interval = setInterval(function(){
+function startslideshow(){
+	loop = setInterval(function(){
+  		if(img_number==total_image){
+			new_img_number=1;
+		}	
+		else
+			new_img_number=img_number+1;
 
-		$(document).keydown(function (e){
-			if(e.keyCode==37){ //left key
-				move="left";
-			}
-  			else if(e.keyCode==39) //right
-  				move="right";
-  		});
-
-  		if(move=="right"){
-  			if(img_number==3)
-				new_img_number=1;
-			else
-				new_img_number=img_number+1;
-			slide_forward(img_number,new_img_number);
-		}
-		else if(move="left"){
-			if(img_number==1)
-				new_img_number=3;
-			else
-			new_img_number=img_number-1;	
-			slide_backward(img_number,new_img_number);
-		}
-
-  		img_number=new_img_number;
-
+		slide_forward();
 
 	},5000);
+};
 
-});
+function slide_forward(){
+		$("img#ikushum_image_"+img_number).hide('slide',{direction:'left'},1000);
+		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'right'},1000);
+		 img_number=new_img_number;
+};
+
+function slide_backward(){
+		$("img#ikushum_image_"+img_number).hide('slide',{direction:'right'},1000);
+		$("img#ikushum_image_"+new_img_number).show('slide',{direction:'left'},1000);
+ 		img_number=new_img_number;
+};
+
+function next(){
+	stoploop();
+	if(img_number==total_image)
+		new_img_number=1;
+	else
+		new_img_number=img_number+1;
+	slide_forward();
+	startslideshow();
+}
+
+function prev(){
+	stoploop();
+	if(img_number==1)
+		new_img_number=total_image;
+	else
+		new_img_number=img_number-1;
+	slide_backward();
+	startslideshow();
+
+}
+
+function stoploop(){
+	window.clearInterval(loop);	
+}
+
+
+
+
+
+
+
